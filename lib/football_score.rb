380aaac4e502bd6206cd854dd1f7071a759928f2
Bartholomew Fountain <baertholomew@gmail.com>
2020-04-25 20:05:24 +0000

    first commit

---
+++ lib/football_score.rb
@@ -0,0 +1,6 @@
+require "football_score/version"
+
+module FootballScore
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/football_score/version.rb
@@ -0,0 +1,3 @@
+module FootballScore
+  VERSION = "0.1.0"
+end
